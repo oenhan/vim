9e7ed34e1660ef26312cb7ba8571b649cba504ea
Huaitong Han <oenhan@gmail.com>
2019-01-08 19:02:50 +0800

    little fix
    Signed-off-by: Huaitong Han <oenhan@gmail.com>

--- init.sh
+++ init.sh
@@ -12,12 +12,12 @@ if [ ! -f "${basepath}/autoload/plug.vim" ];then
     curl -fLo ${basepath}/vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
+rm -fr ~/.vim
+ln -sf ${basepath} ~/.vim
+ln -sf ~/.vim/vimrc ~/.vimrc
+
 vim << EOF
 :PlugUpdate
 :q
 :q
 EOF
-
-rm -fr ~/.vim
-ln -sf ${basepath} ~/.vim
-ln -sf ~/.vim/vimrc ~/.vimrc
